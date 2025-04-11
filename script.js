b927d43dc5c9188448d860b494649d73ff6fc67e
wilnba15 <wsvch05@gmail.com>
2025-04-11 11:20:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.option-btn').forEach(btn => {
+    btn.addEventListener('click', () => {
+        alert('Esta función será habilitada en la versión completa del bot 🤖');
+    });
+});
